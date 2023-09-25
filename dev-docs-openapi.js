d0becaac55002033c8526a08fe8b58e138f0dd6d
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2023-09-25 14:34:59 -0700

    added dev-docs-openapi.js

--- dev-docs-openapi.js
+++ dev-docs-openapi.js
@@ -1,6 +1,10 @@
 const apiConfig = {
+    "openapi": {
+        "specPath": "examples/openapi.yaml",
+        "outputDir": "docs/api/openapi"
+    }
 }
 
- module.exports = {
-    config: apiConfig
- }
+module.exports = {
+  config: apiConfig
+}
