a5a278d732b2f94f21eebe198a1a68518a3c42bc
andrew.ryan.vanbeek@gmail.com <andrewvanbeek@andrews-MacBook-Air-2.local>
2023-09-27 11:32:42 -0700

    changes edit button

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,7 +70,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/team-dev-docs/vectorflow-dev-docs/tree/main/',
         },
         blog: {
           showReadingTime: true,
